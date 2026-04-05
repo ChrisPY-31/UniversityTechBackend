3728a5bb2c2b08c4cd79307ffc01c2efab7ffc67
Christian Pena <christianpeya@hotmail.com>
2026-04-04 20:09:32 -0600

    Initial project setup

---
+++ src/main/java/com/example/nexustechuniversity/NexusTechUniversityApplication.java
@@ -0,0 +1,13 @@
+package com.example.nexustechuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusTechUniversityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusTechUniversityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nexustechuniversity/NexusTechUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nexustechuniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusTechUniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
